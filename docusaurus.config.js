22d7eec4e8ed738f66b6be3601f6b9d6f8ca8e8a
JumpingPufferfish31 <jumpingpufferfish31@gmail.com>
2026-08-30 04:35:38 +0700

    Test deployment trigger

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,8 +38,8 @@ const config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'en-GB',
+    locales: ['en-GB'],
   },
 
   presets: [
